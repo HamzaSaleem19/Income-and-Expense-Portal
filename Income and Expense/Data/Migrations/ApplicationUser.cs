c2dd0f1243108df76369cba2139b086ce45added
HamzaSaleem19 <hamzasaleem.hs778@gmail.com>
2023-03-13 16:40:15 +0500

    Add project files.

---
+++ Income and Expense/Data/Migrations/ApplicationUser.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Income_and_Expense.Data
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string Address { get; set; }
+    }
+}
